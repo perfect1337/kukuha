6442ab1dfdca4b2048cf7a25c880329ebd8289f6
perfect1337 <168024739+perfect1337@users.noreply.github.com>
2024-05-16 16:25:18 +0400

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let switchMode = document.getElementById("switchMode");
+let theme = document.getElementById("theme");
+
+// Проверяем, сохранена ли выбранная тема в localStorage
+const savedTheme = localStorage.getItem('theme');
+
+// Если выбранная тема сохранена, применяем её
+if (savedTheme) {
+    theme.href = savedTheme;
+}
+
 switchMode.onclick = function(){
-    let theme = document.getElementById("theme");
-    if ( theme.getAttribute("href")== "web.css"){
-        theme.href="dark-mode.css";
+    if (theme.getAttribute("href") === "web.css"){
+        theme.href = "dark-mode.css";
+    } else {
+        theme.href = "web.css";
     }
-    else{
-        theme.href="web.css";
-    }
+    
+    // Сохраняем выбранную тему в localStorage
+    localStorage.setItem('theme', theme.href);
 }
